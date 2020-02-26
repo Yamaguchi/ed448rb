ae5cbe18ab42ccb0ad96a7929f665eb76641069b
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2020-02-27 01:44:19 +0900

    Fix specs

--- spec/ed448_spec.rb
+++ spec/ed448_spec.rb
@@ -14,17 +14,18 @@ RSpec.describe Ed448 do
     vectors = JSON.parse(File.read(path), symbolize_names: true)[:vectors]
 
     vectors.each.with_index do |vector, i|
+      
       describe "#{i}:#{vector[:name]}" do
         let(:derived_public_key) { Ed448.derive_public_key(h2b(vector[:private_key])) }
 
         it { expect(b2h(derived_public_key)).to eq vector[:public_key] }
 
-        context = h2b(vector[:context] || '')
-        message = vector[:prehash] ? Ed448::Shake.hash(h2b(vector[:message])) : h2b(vector[:message])
-        signature = Ed448.sign(h2b(vector[:private_key]), h2b(vector[:public_key]), message, context: context, prehash: vector[:prehash])
+        let(:context) { h2b(vector[:context] || '') }
+        let(:message) { vector[:prehash] ? Ed448::Shake.hash(h2b(vector[:message])) : h2b(vector[:message]) }
+        let(:signature) { Ed448.sign(h2b(vector[:private_key]), h2b(vector[:public_key]), message, context: context, prehash: vector[:prehash]) }
         it { expect(b2h(signature)).to eq vector[:signature] }
 
-        result = Ed448.verify(h2b(vector[:signature]), h2b(vector[:public_key]), message, context: context, prehash: vector[:prehash])
+        let(:result) { Ed448.verify(h2b(vector[:signature]), h2b(vector[:public_key]), message, context: context, prehash: vector[:prehash]) }
         it { expect(result).to be_truthy }
       end
     end
